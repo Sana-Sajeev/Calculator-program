cf217992ab826b075151c8b598b5e1b825717488
Sana-Sajeev <sanasajeev41@gmail.com>
2026-08-04 21:39:01 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+
+addition = x+y
+subtraction = x-y
+multiplication = x*y
+division = x/y
+floor_division = x//y
+modulus = x%y
+exponentiation = x**y
+
+print("\n----- Results -----")
+print(f"Addition (+)          = {addition}")
+print(f"Subtraction (-)       = {subtraction}")
+print(f"Multiplication (*)    = {multiplication}")
+print(f"Division (/)          = {division}")
+print(f"Floor Division (//)   = {floor_division}")
+print(f"Modulus (%)           = {modulus}")
+print(f"Exponentiation (**)   = {exponentiation}")
